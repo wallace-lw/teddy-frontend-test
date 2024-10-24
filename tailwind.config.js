2a7772f02b83bf3774cb70989379e92715e15745
wallace-lw <wallacelw22@gmail.com>
2024-10-24 18:52:33 -0300

    feat: :tada: initial project setup

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    darkMode: ["class"],
+    content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],  theme: {
+  	extend: {
+  		colors: {
+  			'teddy-orange': '#EC6724',
+  			'teddy-gray': {
+  				primary: '#D9D9D9',
+  				secondary: '#AAAAAA'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
+  },
+  plugins: [require("tailwindcss-animate")],
+};
