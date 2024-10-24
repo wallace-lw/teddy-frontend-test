aeafa1674ba525e0d2de83a2507bb46cde9cdbfb
wallace-lw <wallacelw22@gmail.com>
2024-10-24 20:31:48 -0300

    chore: :recycle: update importation

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,4 +1,4 @@
-import { LoginForm } from "../../components/LoginForm/LoginForm";
+import { LoginForm } from "@/components";
 export default function LoginPage() {
 	return (
 		<div className="w-full h-dvh flex justify-center items-center bg-teddy-white">
