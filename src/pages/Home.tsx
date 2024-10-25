30cb914d8a93539a362a24c5227c31a7bd384134
wallace-lw <wallacelw22@gmail.com>
2024-10-24 23:39:44 -0300

    refactor: :recycle: updating the home page with the main layout

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import { Customers } from "@/components";
+import { MainLayout } from "@/layouts";
+
+export const Home = () => {
+	return (
+		<MainLayout>
+			<Customers />
+		</MainLayout>
+	);
+};
