2c9f2b209728ecf49812326ddc1b4006fa94eac5
wallace-lw <wallacelw22@gmail.com>
2024-10-24 23:34:59 -0300

    feat: ✨ add  layout component

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,14 @@
+import { Navbar } from "@/components";
+
+interface IProps {
+	children: React.ReactNode;
+}
+
+export const MainLayout: React.FC<IProps> = ({ children }) => {
+	return (
+		<>
+			<Navbar username="Wallace" />
+			{children}
+		</>
+	);
+};
