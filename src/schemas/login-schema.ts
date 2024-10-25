850f7fb67514476b21ce6d799095545b7536fb37
wallace-lw <wallacelw22@gmail.com>
2024-10-25 00:17:00 -0300

    refactor: :sparkles: updating error message from the login schema

--- src/schemas/login-schema.ts
+++ src/schemas/login-schema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const loginSchema = z.object({
-  name: z.string().min(2, "O nome precisa ter no mínimo duas letras"),
+  name: z.string().min(2, "O nome precisa ter no mínimo dois caracteres"),
 })
 
 
