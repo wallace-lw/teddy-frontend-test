3a8477d2af75552bd0403060a59ab0a7bf0a9e81
wallace-lw <wallacelw22@gmail.com>
2024-10-24 23:40:02 -0300

    feat: :sparkles: add login page

---
+++ src/pages/Login.tsx
@@ -0,0 +1,8 @@
+import { LoginForm } from "@/components";
+export const LoginPage = () => {
+	return (
+		<div className="w-full h-dvh flex justify-center items-center bg-teddy-white">
+			<LoginForm />
+		</div>
+	);
+};
