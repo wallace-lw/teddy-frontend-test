99a1612be6dc40d97a0ffbde8a1fa7cf59a90f22
wallace-lw <wallacelw22@gmail.com>
2024-10-25 00:15:48 -0300

    refactor: :recycle: implements auth provider in the project root

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { AppRouter } from "./routes/Provider.tsx";
+import { AuthProvider } from "./contexts/AuthContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<AppRouter />
+		<AuthProvider>
+			<AppRouter />
+		</AuthProvider>
 	</StrictMode>,
 );
