38602b65d7c7e865c62bf0b3b18597fb297ea100
wallace-lw <wallacelw22@gmail.com>
2024-10-24 23:29:06 -0300

    feat: :sparkles: add create customer dialog

---
+++ src/components/CreateCustomerDialog/CreateCustomerDialog.tsx
@@ -0,0 +1,98 @@
+import { PlusCircle } from "lucide-react";
+import { Button } from "../ui/button";
+import {
+	Dialog,
+	DialogContent,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "../ui/dialog";
+import { Input } from "../ui/input";
+import { CustomerSchema, customerSchema } from "../../schemas/customer-schema";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ErrorMessage } from "../ErrorMessage";
+import { cn } from "@/lib/utils";
+
+export const CreateCustomerDialog = () => {
+	const {
+		handleSubmit,
+		register,
+		formState: { errors },
+	} = useForm<CustomerSchema>({
+		resolver: zodResolver(customerSchema),
+	});
+
+	const onSubmit = (data: CustomerSchema) => {
+		console.log(data);
+	};
+
+	return (
+		<Dialog>
+			<DialogTrigger>
+				<Button className="bg-transparent border border-teddy-orange text-teddy-orange font-semibold w-full lg:w-[200px] hover:bg-teddy-orange hover:text-white transition-all">
+					<PlusCircle />
+					Novo cliente
+				</Button>
+			</DialogTrigger>
+			<DialogContent>
+				<DialogHeader>
+					<DialogTitle>Novo cliente</DialogTitle>
+				</DialogHeader>
+				<form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
+					<Input
+						type="text"
+						placeholder="Digite o nome:"
+						autoComplete="off"
+						{...register("name")}
+						className={cn(
+							"h-12 px-2  border-teddy-gray-primary  placeholder:text-teddy-gray-secondary",
+							errors.name?.message
+								? "focus-visible:ring-red-700"
+								: "focus-visible:ring-teddy-orange",
+						)}
+					/>
+					{errors.name?.message && (
+						<ErrorMessage message={errors.name.message} />
+					)}
+					<Input
+						type="text"
+						placeholder="Digite o salário:"
+						autoComplete="off"
+						{...register("salary")}
+						className={cn(
+							"h-12 px-2  border-teddy-gray-primary  placeholder:text-teddy-gray-secondary",
+							errors.name?.message
+								? "focus-visible:ring-red-700"
+								: "focus-visible:ring-teddy-orange",
+						)}
+					/>
+					{errors.salary?.message && (
+						<ErrorMessage message={errors.salary.message} />
+					)}
+					<Input
+						type="text"
+						placeholder="Digite o valor da empresa:"
+						autoComplete="off"
+						{...register("companyValue")}
+						className={cn(
+							"h-12 px-2  border-teddy-gray-primary  placeholder:text-teddy-gray-secondary",
+							errors.name?.message
+								? "focus-visible:ring-red-700"
+								: "focus-visible:ring-teddy-orange",
+						)}
+					/>
+					{errors.companyValue?.message && (
+						<ErrorMessage message={errors.companyValue.message} />
+					)}
+					<Button
+						className="bg-teddy-orange text-white font-semibold"
+						type="submit"
+					>
+						Criar cliente
+					</Button>
+				</form>
+			</DialogContent>
+		</Dialog>
+	);
+};
