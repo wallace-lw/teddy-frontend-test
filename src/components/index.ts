e7b0fb8c34fc3b511c81e998c187c75a0989da7b
wallace-lw <wallacelw22@gmail.com>
2024-10-24 23:30:49 -0300

    chore: :recycle: update the barrel pattern importation from the components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,8 @@
 export * from './ErrorMessage'
 export * from './LoginForm'
 export * from './Navbar'
+export * from './CustomerCard'
+export * from './Customers'
+export * from './CreateCustomerDialog'
+export * from './DeleteCustomerDialog'
 
