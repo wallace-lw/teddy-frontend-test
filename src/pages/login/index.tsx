6d538a1f00ab4fff4aa46d6aa4cdabbddae9fea9
wallace-lw <wallacelw22@gmail.com>
2024-10-24 23:41:15 -0300

    chore: :fire: removing uncessary importation file

--- src/pages/login/index.tsx
+++
@@ -1,8 +0,0 @@
-import { LoginForm } from "@/components";
-export default function LoginPage() {
-	return (
-		<div className="w-full h-dvh flex justify-center items-center bg-teddy-white">
-			<LoginForm />
-		</div>
-	);
-}
