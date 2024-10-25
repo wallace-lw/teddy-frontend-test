03c77da0f6718464c115f26b0b733631d5a8e79b
wallace-lw <wallacelw22@gmail.com>
2024-10-24 23:42:02 -0300

    refactor: :recycle: update router

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,14 +1,19 @@
 import App from "@/App";
-import LoginPage from "@/pages/login";
+import { LoginPage } from "@/pages";
+import { SelectedCustomers } from "@/pages/SelectedCustomers";
 import { createBrowserRouter } from "react-router-dom";
 
 export const router = createBrowserRouter([
 	{
 		path: "/",
+		element: <LoginPage />,
+	},
+	{
+		path: "/clientes",
 		element: <App />,
 	},
 	{
-		path: "/login",
-		element: <LoginPage />,
+		path: "/clientes-selecionados",
+		element: <SelectedCustomers />,
 	},
 ]);
