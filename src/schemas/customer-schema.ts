7b3bfc1f4cb93ecbcce0cb3a4c1746f4a51f5057
wallace-lw <wallacelw22@gmail.com>
2024-10-25 15:55:31 -0300

    refactor: :recycle: updating the company valuation field

--- src/schemas/customer-schema.ts
+++ src/schemas/customer-schema.ts
@@ -1,9 +1,13 @@
 import { z } from "zod";
 
 export const customerSchema = z.object({
-  name: z.string().min(2, "O nome deverá ter pelo menos dois caracteres"),
-  salary: z.string().min(4,"O salário deverá ter pelo menos quatro caracteres"),
-  companyValue: z.string().min(5,"O valor da empres deverá ter pelo menos cinco caracteres")
-})
+	name: z.string().min(2, "O nome deverá ter pelo menos dois caracteres"),
+	salary: z
+		.string()
+		.min(4, "O salário deverá ter pelo menos quatro caracteres"),
+	companyValuation: z
+		.string()
+		.min(5, "O valor da empres deverá ter pelo menos cinco caracteres"),
+});
 
-export type CustomerSchema = z.infer<typeof customerSchema>
+export type CustomerSchema = z.infer<typeof customerSchema>;
