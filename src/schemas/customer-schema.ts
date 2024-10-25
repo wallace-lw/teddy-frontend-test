4fd51376e5a9e747ca52dff24a24f2b776011a98
wallace-lw <wallacelw22@gmail.com>
2024-10-24 23:40:53 -0300

    feat: :sparkles: add customer schema

---
+++ src/schemas/customer-schema.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+export const customerSchema = z.object({
+  name: z.string().min(2, "O nome deverá ter pelo menos dois caracteres"),
+  salary: z.string().min(4,"O salário deverá ter pelo menos quatro caracteres"),
+  companyValue: z.string().min(5,"O valor da empres deverá ter pelo menos cinco caracteres")
+})
+
+export type CustomerSchema = z.infer<typeof customerSchema>
