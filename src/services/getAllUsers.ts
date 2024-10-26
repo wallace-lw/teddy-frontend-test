a3c1ded3f60e0e987551afd0add4ef6db7a55ddd
wallace-lw <wallacelw22@gmail.com>
2024-10-25 23:50:44 -0300

    refactor: :recycle: changing the api url from manuel to enviroment variable

--- src/services/getAllUsers.ts
+++ src/services/getAllUsers.ts
@@ -1,4 +1,5 @@
 import { IAllUsersResponse } from "@/interfaces";
+import { API_URL } from "@/utils/constants";
 
 interface IProps {
 	page?: number;
@@ -9,13 +10,10 @@ export const getAllUsers = async ({
 	page = 1,
 	limit = 16,
 }: IProps): Promise<IAllUsersResponse> => {
-	const response = await fetch(
-		`https://cors-anywhere.herokuapp.com/https://boasorte.teddybackoffice.com.br/users?page=${page}&limit=${limit}`,
-		{
-			method: "GET",
-			mode: "cors",
-		},
-	);
+	const response = await fetch(`${API_URL}/users?page=${page}&limit=${limit}`, {
+		method: "GET",
+		mode: "cors",
+	});
 
 	const data: IAllUsersResponse = await response.json();
 	console.log(data);
--- src/utils/constants/env.ts
+++ src/utils/constants/env.ts
@@ -1,1 +1,3 @@
-export const API_URL: string = "https://boasorte.teddybackoffice.com.br/";
+export const API_URL: string =
+	(import.meta.env.VITE_API_URL as string) ||
+	"https://cors-anywhere.herokuapp.com/https://boasorte.teddybackoffice.com.br";
