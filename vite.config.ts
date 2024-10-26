d55d46fcf6eb802af8595545caf5e1123af38376
wallace-lw <wallacelw22@gmail.com>
2024-10-25 23:32:06 -0300

    feat: :sparkles: add the mkcert for the https protocol

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import path from "node:path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		port: 3000,
-	},
+	plugins: [react(), mkcert()],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
