856a1ada7c3c5a6409b59e9fbe6dc76a5a1bb380
wallace-lw <wallacelw22@gmail.com>
2024-10-26 14:32:36 -0300

    fix: :bug: fixing proxy bug

--- api/proxy.js
+++ api/proxy.js
@@ -1,31 +1,28 @@
 export default async function handler(req, res) {
+  
   try {
-    const baseUrl = new URL(req.url, `http://${req.headers.host}`);
-    const targetUrl = baseUrl.searchParams.get('url');
+    const url = decodeURIComponent(req.url.split('/api/proxy?')[1])
 
-    if (!targetUrl) {
-      return res.status(400).json({ message: "Missing target URL" });
-    }
+    const bodyContent = req.method === 'POST' | req.method === 'POST' ? req.body : undefined;
 
-    const response = await fetch(targetUrl, {
+    const response = await fetch(url, {
       method: req.method,
       headers: {
         ...req.headers,
-        'Content-Type': 'application/json',
+        'Origin': req.headers.origin,
       },
-      body: ['POST', 'PATCH'].includes(req.method) ? JSON.stringify(req.body) : null,
+      body: bodyContent ? JSON.stringify(bodyContent) : null,
     });
-
+    
     const data = await response.json();
 
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,DELETE,PATCH');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Content-Type', 'application/json');
-
+    
     res.status(200).json(data);
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Error fetching the URL' });
+    res.status(500).send('Error fetching the URL');
   }
 }
