18cced1ba950294b96c850c50e1ef3dd892991ca
wallace-lw <wallacelw22@gmail.com>
2024-10-26 13:37:07 -0300

    refactor: :recycle: updating proxy

--- api/proxy.js
+++ api/proxy.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
     const data = await response.json();
 
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,DELETE,PATCH');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Content-Type', 'application/json');
     
