a1a54ed79ea9e9c738b477cc5121b69179a51d57
wallace-lw <wallacelw22@gmail.com>
2024-10-25 23:30:50 -0300

    refactor: :recycle: update importation on the main.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { AppRouter } from "./routes/Provider.tsx";
-import { Providers } from "./utils/Providers.tsx";
+import { AppRouter } from "./routes";
+import { Providers } from "./utils";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
