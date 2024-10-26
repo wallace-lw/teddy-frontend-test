a5af80b0050f636dc923ae42b1779f1747b1237d
wallace-lw <wallacelw22@gmail.com>
2024-10-26 14:19:44 -0300

    fix: :bug: fixing bug

--- api/proxy.js
+++ api/proxy.js
@@ -1,25 +1,31 @@
 export default async function handler(req, res) {
-  const url = decodeURIComponent(req.url.split('/api/proxy?')[1])
-
   try {
-    const response = await fetch(url, {
+    const baseUrl = new URL(req.url, `http://${req.headers.host}`);
+    const targetUrl = baseUrl.searchParams.get('url');
+
+    if (!targetUrl) {
+      return res.status(400).json({ message: "Missing target URL" });
+    }
+
+    const response = await fetch(targetUrl, {
       method: req.method,
       headers: {
         ...req.headers,
-        'Origin': req.headers.origin,
+        'Content-Type': 'application/json',
       },
-      body: req.method === 'POST' ? JSON.stringify(req.body) : null,
+      body: ['POST', 'PATCH'].includes(req.method) ? JSON.stringify(req.body) : null,
     });
-    
+
     const data = await response.json();
 
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,DELETE,PATCH');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Content-Type', 'application/json');
-    
+
     res.status(200).json(data);
   } catch (error) {
-    res.status(500).send('Error fetching the URL');
+    console.error(error);
+    res.status(500).json({ message: 'Error fetching the URL' });
   }
 }
