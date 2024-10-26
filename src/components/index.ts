74b03081296ebadf931b12ed3b6525a47e32eb9b
wallace-lw <wallacelw22@gmail.com>
2024-10-26 04:11:05 -0300

    chore: updating formatation

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,7 @@
-export * from './ErrorMessage'
-export * from './LoginForm'
-export * from './Navbar'
-export * from './CustomerCard'
-export * from './Customers'
-export * from './CreateCustomerDialog'
-export * from './DeleteCustomerDialog'
-
+export * from "./ErrorMessage";
+export * from "./LoginForm";
+export * from "./Navbar";
+export * from "./CustomerCard";
+export * from "./Customers";
+export * from "./CreateCustomerDialog";
+export * from "./DeleteCustomerDialog";
