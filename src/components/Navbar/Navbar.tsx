8c60da3d8589bc9a56c447d9c1643d274d9a24cc
wallace-lw <wallacelw22@gmail.com>
2024-10-27 03:54:34 -0300

    feat: :sparkles: add the sidebar functions from the context

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,28 +4,38 @@ import { Button } from "../ui/button";
 import { links } from "@/utils/constants";
 import { NavbarLinks } from "./NavbarLinks";
 import { useAuthContext } from "@/contexts/AuthContext";
+import { Sidebar } from "@/components";
+import { useSidebarContext } from "@/contexts";
 
 export const Navbar: React.FC = () => {
 	const { logout, username } = useAuthContext();
 
+	const { toggleSidebar } = useSidebarContext();
+
 	return (
-		<nav className="flex px-20 py-4 items-center justify-between border-b border-b-teddy-gray-primary">
-			<div className="flex items-center gap-6 ju">
-				<Menu className="block md:hidden" />
-				<img src={teddyLogo} alt="logo" />
-			</div>
-			<NavbarLinks links={links} />
-			<div className="items-center gap-4 hidden md:flex">
-				<span>
-					Olá, <strong>{username}</strong>
-				</span>
-				<Button
-					className="rounded-full size-7 bg-teddy-orange text-white"
-					onClick={logout}
-				>
-					<LogOutIcon />
-				</Button>
-			</div>
-		</nav>
+		<>
+			<Sidebar />
+			<nav className="flex px-10 py-4 items-center justify-between border-b border-b-teddy-gray-primary">
+				<div className="flex items-center gap-6">
+					<Menu
+						className="block md:hidden cursor-pointer"
+						onClick={toggleSidebar}
+					/>
+					<img src={teddyLogo} alt="logo" className="mx-auto" />
+				</div>
+				<NavbarLinks links={links} />
+				<div className="items-center gap-4 hidden md:flex">
+					<span>
+						Olá, <strong>{username}</strong>
+					</span>
+					<Button
+						className="rounded-full size-7 bg-teddy-orange text-white"
+						onClick={logout}
+					>
+						<LogOutIcon />
+					</Button>
+				</div>
+			</nav>
+		</>
 	);
 };
